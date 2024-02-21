37c00498f029e95e392be0754cf9964eef71f061
OliWalker01 <103366754+OliWalker01@users.noreply.github.com>
2024-02-21 21:29:12 +0000

    Started making Marquee Matchups Minigame

---
+++ Source/MindScribe/Private/MarqueeMatchUps.cpp
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MarqueeMatchUps.h"
+
+// Sets default values
+AMarqueeMatchUps::AMarqueeMatchUps()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMarqueeMatchUps::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	MaxCards = 1;
+}
+
+// Called every frame
+void AMarqueeMatchUps::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Generates a Pattern
+FString AMarqueeMatchUps::GeneratePattern()
+{
+	PickShape();
+
+	return Shape;
+}
+
+// Picks a Shape to Add to Pattern
+void AMarqueeMatchUps::PickShape()
+{
+	srand(static_cast<unsigned int>(time(0)));
+	int random = rand() % 4;
+
+	switch (random)
+	{
+	case(0):
+		Shape = "Circle";
+		break;
+	case(1):
+		Shape = "Heart";
+		break;
+	case(2):
+		Shape = "Square";
+		break;
+	case(3):
+		Shape = "Star";
+		break;
+	case(4):
+		Shape = "Triangle";
+		break;
+	default:
+		Shape = NULL;
+		break;
+	}
+}
---
+++ Source/MindScribe/Public/MarqueeMatchUps.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MarqueeMatchUps.generated.h"
+
+using namespace std;
+
+UCLASS()
+class MINDSCRIBE_API AMarqueeMatchUps : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMarqueeMatchUps();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(BlueprintReadWrite, Category = Game)
+		int MaxCards;
+	UPROPERTY(BlueprintReadWrite, Category = Game)
+		TArray<FString> Pattern;
+	UPROPERTY(BlueprintReadWrite, Category = Game)
+		FString Shape;
+
+	UFUNCTION(BlueprintCallable, Category = Game)
+		FString GeneratePattern();
+
+	void PickShape();
+};
