b0b1a11ca675aadb79c81684a7f5b08e03824106
OliWalker01 <103366754+OliWalker01@users.noreply.github.com>
2024-03-04 19:54:37 +0000

    Worked on the fundamentals for the high score system

---
+++ Source/MindScribe/Public/MindscribeSaveGame.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "MindscribeSaveGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MINDSCRIBE_API UMindscribeSaveGame : public USaveGame
+{
+	GENERATED_BODY()
+	
+};
