71e7308a08c454eb42a6da31645dc1f6a2faaad7
OliWalker01 <103366754+OliWalker01@users.noreply.github.com>
2024-03-23 19:16:10 +0000

    Added systems to more advance sudoku template

--- Source/MindScribe/Private/Sudoku.cpp
+++ Source/MindScribe/Private/Sudoku.cpp
@@ -27,12 +27,10 @@ void ASudoku::Tick(float DeltaTime)
 
 void ASudoku::PickSudokuEasyQuestion()
 {
-	numberOfLives = 3;
+	numberOfLives = 5;
 
-	/*srand(static_cast<unsigned int>(time(0)));
+	srand(static_cast<unsigned int>(time(0)));
 	int questionNumber = rand() & 3;
-
-	questionNumber = 0;
 
 	switch (questionNumber)
 	{
@@ -48,9 +46,35 @@ void ASudoku::PickSudokuEasyQuestion()
 	case 3:
 		SudokuEasyQuestionFour();
 		break;
-	}*/
+	}
 
-	SudokuEasyQuestionOne();
+	//SudokuEasyQuestionOne();
+}
+
+void ASudoku::PickSudokuMediumQuestion()
+{
+	numberOfLives = 3;
+
+	srand(static_cast<unsigned int>(time(0)));
+	int questionNumber = rand() & 3;
+
+	switch (questionNumber)
+	{
+	case 0:
+		SudokuMediumQuestionOne();
+		break;
+	case 1:
+		//02SudokuMediumQuestionTwo();
+		break;
+	case 2:
+		//SudokuMediumQuestionThree();
+		break;
+	case 3:
+		//SudokuMediumQuestionFour();
+		break;
+	}
+
+	//SudokuMediumQuestionOne();
 }
 
 void ASudoku::SudokuEasyQuestionOne()
@@ -92,6 +116,123 @@ void ASudoku::SudokuEasyQuestionOne()
 	correctAnswers = 4;
 }
 
+void ASudoku::SudokuEasyQuestionTwo()
+{
+	A1 = 3;
+	A2 = 4;
+	A3 = 2;
+	A4 = 1;
+	B1 = 1;
+	B2 = 2;
+	B3 = 3;
+	B4 = 4;
+	C1 = 2;
+	C2 = 1;
+	C3 = 4;
+	C4 = 3;
+	D1 = 4;
+	D2 = 3;
+	D3 = 1;
+	D4 = 2;
+
+	A1Active = true;
+	A2Active = false;
+	A3Active = true;
+	A4Active = true;
+	B1Active = true;
+	B2Active = true;
+	B3Active = false;
+	B4Active = true;
+	C1Active = false;
+	C2Active = true;
+	C3Active = true;
+	C4Active = true;
+	D1Active = true;
+	D2Active = true;
+	D3Active = true;
+	D4Active = false;
+
+	correctAnswers = 4;
+}
+
+void ASudoku::SudokuEasyQuestionThree()
+{
+	A1 = 3;
+	A2 = 2;
+	A3 = 4;
+	A4 = 1;
+	B1 = 1;
+	B2 = 4;
+	B3 = 3;
+	B4 = 2;
+	C1 = 2;
+	C2 = 3;
+	C3 = 1;
+	C4 = 4;
+	D1 = 4;
+	D2 = 1;
+	D3 = 2;
+	D4 = 3;
+
+	A1Active = false;
+	A2Active = true;
+	A3Active = false;
+	A4Active = true;
+	B1Active = true;
+	B2Active = true;
+	B3Active = true;
+	B4Active = true;
+	C1Active = true;
+	C2Active = true;
+	C3Active = true;
+	C4Active = true;
+	D1Active = true;
+	D2Active = false;
+	D3Active = true;
+	D4Active = false;
+
+	correctAnswers = 4;
+}
+
+void ASudoku::SudokuEasyQuestionFour()
+{
+	A1 = 4;
+	A2 = 1;
+	A3 = 2;
+	A4 = 3;
+	B1 = 3;
+	B2 = 2;
+	B3 = 1;
+	B4 = 4;
+	C1 = 1;
+	C2 = 4;
+	C3 = 3;
+	C4 = 2;
+	D1 = 2;
+	D2 = 3;
+	D3 = 4;
+	D4 = 1;
+
+	A1Active = false;
+	A2Active = true;
+	A3Active = true;
+	A4Active = true;
+	B1Active = true;
+	B2Active = true;
+	B3Active = false;
+	B4Active = true;
+	C1Active = true;
+	C2Active = true;
+	C3Active = false;
+	C4Active = true;
+	D1Active = true;
+	D2Active = false;
+	D3Active = false;
+	D4Active = true;
+
+	correctAnswers = 4;
+}
+
 void ASudoku::QuitGame()
 {
 	APlayerController* PlayerController = GetWorld()->GetFirstPlayerController();
@@ -105,4 +246,83 @@ void ASudoku::QuitGame()
 void ASudoku::AddToNotedNumbers(FString ActiveCard)
 {
 
+}
+
+void ASudoku::SudokuMediumQuestionOne()
+{
+	A1 =
+	A2 = 
+	A3 = 
+	A4 = 
+	A5 = 
+	A6 =
+	B1 = 
+	B2 = 
+	B3 = 
+	B4 = 
+	B5 = 
+	B6 = 
+	C1 = 
+	C2 = 
+	C3 = 
+	C4 = 
+	C5 = 
+	C6 = 
+	D1 = 
+	D2 = 
+	D3 = 
+	D4 = 
+	D5 = 
+	D6 = 
+	E1 = 
+	E2 = 
+	E3 = 
+	E4 = 
+	E5 = 
+	E6 = 
+	F1 = 
+	F2 = 
+	F3 = 
+	F4 = 
+	F5 = 
+	F6 = 
+
+	A1Active =
+	A2Active =
+	A3Active =
+	A4Active =
+	A5Active =
+	A6Active =
+	B1Active =
+	B2Active =
+	B3Active =
+	B4Active =
+	B5Active =
+	B6Active =
+	C1Active =
+	C2Active =
+	C3Active =
+	C4Active =
+	C5Active =
+	C6Active =
+	D1Active =
+	D2Active =
+	D3Active =
+	D4Active =
+	D5Active =
+	D6Active =
+	E1Active =
+	E2Active =
+	E3Active =
+	E4Active =
+	E5Active =
+	E6Active =
+	F1Active =
+	F2Active =
+	F3Active =
+	F4Active =
+	F5Active =
+	F6Active =
+
+	correctAnswers = 
 }
--- Source/MindScribe/Public/Sudoku.h
+++ Source/MindScribe/Public/Sudoku.h
@@ -33,6 +33,10 @@ public:
 		int A3;
 	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
 		int A4;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		int A5;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		int A6;
 
 	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
 		TArray <int> A1NotedNumbers;
@@ -42,6 +46,10 @@ public:
 		TArray <int> A3NotedNumbers;
 	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
 		TArray <int> A4NotedNumbers;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		TArray <int> A5NotedNumbers;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		TArray <int> A6NotedNumbers;
 
 	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
 		bool A1Active;
@@ -51,6 +59,10 @@ public:
 		bool A3Active;
 	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
 		bool A4Active;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		bool A5Active;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		bool A6Active;
 
 	// Row B
 	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
@@ -61,6 +73,10 @@ public:
 		int B3;
 	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
 		int B4;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		int B5;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		int B6;
 
 	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
 		TArray <int> B1NotedNumbers;
@@ -70,6 +86,10 @@ public:
 		TArray <int> B3NotedNumbers;
 	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
 		TArray <int> B4NotedNumbers;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		TArray <int> B5NotedNumbers;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		TArray <int> B6NotedNumbers;
 
 
 	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
@@ -80,6 +100,10 @@ public:
 		bool B3Active;
 	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
 		bool B4Active;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		bool B5Active;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		bool B6Active;
 
 	// Row C
 	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
@@ -90,6 +114,10 @@ public:
 		int C3;
 	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
 		int C4;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		int C5;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		int C6;
 
 	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
 		TArray <int> C1NotedNumbers;
@@ -99,6 +127,10 @@ public:
 		TArray <int> C3NotedNumbers;
 	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
 		TArray <int> C4NotedNumbers;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		TArray <int> C5NotedNumbers;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		TArray <int> C6NotedNumbers;
 
 	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
 		bool C1Active;
@@ -108,6 +140,10 @@ public:
 		bool C3Active;
 	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
 		bool C4Active;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		bool C5Active;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		bool C6Active;
 
 	// Row D
 	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
@@ -118,6 +154,10 @@ public:
 		int D3;
 	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
 		int D4;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		int D5;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		int D6;
 
 	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
 		TArray <int> D1NotedNumbers;
@@ -127,6 +167,10 @@ public:
 		TArray <int> D3NotedNumbers;
 	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
 		TArray <int> D4NotedNumbers;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		TArray <int> D5NotedNumbers;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		TArray <int> D6NotedNumbers;
 
 	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
 		bool D1Active;
@@ -136,6 +180,90 @@ public:
 		bool D3Active;
 	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
 		bool D4Active;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		bool D5Active;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		bool D6Active;
+
+	// Row E
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		int E1;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		int E2;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		int E3;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		int E4;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		int E5;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		int E6;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		TArray <int> E1NotedNumbers;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		TArray <int> E2NotedNumbers;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		TArray <int> E3NotedNumbers;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		TArray <int> E4NotedNumbers;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		TArray <int> E5NotedNumbers;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		TArray <int> E6NotedNumbers;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		bool E1Active;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		bool E2Active;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		bool E3Active;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		bool E4Active;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		bool E5Active;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		bool E6Active;
+
+	// Row F
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		int F1;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		int F2;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		int F3;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		int F4;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		int F5;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		int F6;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		TArray <int> F1NotedNumbers;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		TArray <int> F2NotedNumbers;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		TArray <int> F3NotedNumbers;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		TArray <int> F4NotedNumbers;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		TArray <int> F5NotedNumbers;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		TArray <int> F6NotedNumbers;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		bool F1Active;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		bool F2Active;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		bool F3Active;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		bool F4Active;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		bool F5Active;
+	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
+		bool F6Active;
 
 	UPROPERTY(BlueprintReadWrite, Category = "Sudoku")
 		int correctAnswers;
@@ -145,6 +273,8 @@ public:
 
 	UFUNCTION(BlueprintCallable, Category = "Sudoku")
 		void PickSudokuEasyQuestion();
+	UFUNCTION(BlueprintCallable, Category = "Sudoku")
+		void PickSudokuMediumQuestion();
 
 	UFUNCTION(BlueprintCallable, Category = "Sudoku")
 		void AddToNotedNumbers(FString ActiveCard);
@@ -153,6 +283,8 @@ public:
 	void SudokuEasyQuestionTwo();
 	void SudokuEasyQuestionThree();
 	void SudokuEasyQuestionFour();
+
+	void SudokuMediumQuestionOne();
 
 	void QuitGame();
 };
